4b16a51ccb4c45a8cb1e0cc36623b1124979403f
Jack Allen <jack.allen@konsentus.com>
2023-10-13 14:36:16 +0100

    Added action logic

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const lodash = require('lodash');
+
+const pathToFile = core.getInput('path-to-file');
+const environments = core.getMultilineInput('environments');
+
+const globalEnvironment = 'global';
+
+// read config file
+const file = fs.readFileSync(pathToFile, 'utf-8');
+
+// convert from yaml to JS
+const config = yaml.load(file);
+
+const allEnvironments = [globalEnvironment, ...environments];
+
+const configs = allEnvironments.map(configToMerge => config[configToMerge] || {});
+const mergedConfig = lodash.merge(configs);
+
+core.debug(`Final merged config is ${JSON.stringify(mergedConfig)}`);
+
+Array.from(Object.entries(mergedConfig)).forEach(([key, value]) => core.exportVariable(key, value));
